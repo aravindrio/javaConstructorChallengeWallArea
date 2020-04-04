9a6584c168d6583cc95af6fee4bf88533b11d0a8
aravindrio <aravind.gtr@gmail.com>
2020-04-04 13:39:55 -0700

    Initial commit

---
+++ src/com/aravind/Main.java
@@ -0,0 +1,17 @@
+package com.aravind;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Wall wall = new Wall(5, 4);
+        System.out.println("Area " + wall.getArea());
+
+        wall.setHeight(-1.5);
+        System.out.println("Width= " + wall.getWidth());
+        System.out.println("Height= " + wall.getHeight());
+        System.out.println("Area= " + wall.getArea());
+
+
+    }
+}
